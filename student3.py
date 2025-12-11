b044f641a5d9f4fe04292e982339dada22f21add
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2025-12-11 21:27:04 +0530

    Update student3.py

--- student3.py
+++ student3.py
@@ -1,5 +1,13 @@
 import sys
-marks = list(map(float, sys.argv[1:]))
+
+default_marks = [85, 90, 78, 88, 92]
+
+if len(sys.argv) == 6:   # 5 marks + script name
+    marks = list(map(float, sys.argv[1:]))
+    source = "User Input"
+else:
+    marks = default_marks
+    source = "Self-assigned"
 
 if len(marks) != 5:
     print("Error: Please provide exactly 5 marks.")
@@ -18,5 +26,6 @@ elif average >= 40:
 else:
     grade = "Fail"
 
+print(f"Marks: {marks} ({source})")
 print(f"Average Marks: {average:.2f}")
 print(f"Grade: {grade}")
